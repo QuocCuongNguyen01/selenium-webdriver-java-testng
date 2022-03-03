0f1c45a97f320bf02bcc0f91cd608d1efac069ac
Cuong Nguyen <cuongdnqb5@gmail.com>
2022-03-03 22:21:15 +0700

    Selenium_locator

--- selenium/webdriver/Topic_00_template.java
+++ selenium/webdriver/Topic_00_template.java
@@ -4,13 +4,14 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_01_Check_Environment {
+public class Topic_00_template {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 
@@ -19,8 +20,8 @@ public class Topic_01_Check_Environment {
 		System.setProperty("webdriver.gecko.driver", projectPath + "/browserDriver/geckodriver");
 		driver = new FirefoxDriver();
 		
-		//System.setProperty("webdriver.chrome.driver", projectPath + "/browserDriver/Chromedriver");
-		//driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", projectPath + "/browserDriver/Chromedriver");
+		driver = new ChromeDriver();
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
@@ -29,23 +30,15 @@ public class Topic_01_Check_Environment {
 	}
 
 	@Test
-	public void TC_01_ValidateCurrentUrl() {
-		// Login Page Url matching
-		String loginPageUrl = driver.getCurrentUrl();
-		Assert.assertEquals(loginPageUrl, "https://www.facebook.com/");
+	public void TC_01() {
+		
 	}
-
 	@Test
 	public void TC_02_ValidatePageTitle() {
-		// Login Page title
-		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
+		
 	}
-
 	@Test
 	public void TC_03_LoginFormDisplayed() {
-		// Login form displayed
-		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
 	}
 
 	@AfterClass
---
+++ selenium/webdriver/Topic_02_Selenium_locator.java
@@ -0,0 +1,85 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_02_Selenium_locator {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "/browserDriver/geckodriver");
+		driver = new FirefoxDriver();
+		
+		//System.setProperty("webdriver.chrome.driver", projectPath + "/browserDriver/Chromedriver");
+		//driver = new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://stackoverflow.com/users/login?ssrc=user_prefs&returnurl=https%3a%2f%2fstackoverflow.com%2fusers%2fpreferences%2fcurrent");
+	
+	}
+
+	@Test
+	public void TC_01() {
+		//id
+		driver.findElement(By.id("email"));
+		//Class
+		driver.findElement(By.className("s-input"));
+		//Name
+		driver.findElement(By.name("email"));
+		//Tagname: Tìm xem có bao nhiêu element cùng loại trên HLML
+		driver.findElement(By.tagName("input"));
+		//Linktext:Truyển cả text vào
+		driver.findElement(By.linkText("Sign up"));
+		//Partial link text: 1 Phần của text
+		driver.findElement(By.partialLinkText("Sign"));
+		
+		
+		//CSS
+		driver.findElement(By.cssSelector("input[id='email']"));
+		//driver.findElement(By.cssSelector("input#email]"));
+		//driver.findElement(By.cssSelector("#email]"));
+		
+		driver.findElement(By.cssSelector("input[class='s-input']"));
+		driver.findElement(By.cssSelector("input.s-input"));
+		driver.findElement(By.cssSelector(".s-input"));
+		
+		driver.findElement(By.cssSelector("input[name='email']"));
+		
+		driver.findElement(By.cssSelector("input"));
+		
+		//Xpath
+		driver.findElement(By.xpath("//input[@id='email']"));
+		driver.findElement(By.xpath("//input[@class='s-input']"));
+		driver.findElement(By.xpath("//input[contains(@class,'s-inpu')]"));
+		//driver.findElement(By.xpath("//input[start-with(@class,'s-inpu')]"));
+		driver.findElement(By.xpath("//input[@name='email']"));
+		driver.findElement(By.xpath("//input"));
+		//driver.findElement(By.xpath("//a[@text='Sign up']"));
+		//driver.findElement(By.xpath("//a[contains(@text(),'Sign')]"));
+		//driver.findElement(By.xpath("//a[contains(@text(),'up')]"));
+		
+		
+		
+	}
+	@Test
+	public void TC_02() {
+		
+	}
+	@Test
+	public void TC_03() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
