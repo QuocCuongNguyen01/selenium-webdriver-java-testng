ec33f1363b3c93cd334a01f81d27accbd51b7034
Cuong Nguyen <cuongdnqb5@gmail.com>
2022-05-09 15:57:03 +0700

    update_Topic_10

--- selenium/webdriver/Topic_10_Handle_Custom_Dropdown.java
+++ selenium/webdriver/Topic_10_Handle_Custom_Dropdown.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -17,15 +18,18 @@ import org.testng.annotations.Test;
 
 public class Topic_10_Handle_Custom_Dropdown {
 	WebDriver driver;
+	Select select;
+	String tinh, khuvuc;
 	WebDriverWait explicitWait;
 	String projectPath = System.getProperty("user.dir");
 
 	@BeforeClass
 	public void beforeClass() {
 		// Mở browser lên
-				System.setProperty("webdriver.gecko.driver", projectPath + "/browserDriver/geckodriver");
-				driver = new FirefoxDriver();
-				
+				//System.setProperty("webdriver.gecko.driver", projectPath + "/browserDriver/geckodriver");
+				//	driver = new FirefoxDriver();
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDriver/Chromedriver");
+			driver = new ChromeDriver();
 				// Luôn khởi tạo sau driver = > cần giá trị driver để khởi tạo explicitwait lên
 				// Wait cho các element theo điều kiện có sẵn :	visible/ invisible/ presence/ clickable/ ....
 				explicitWait = new WebDriverWait(driver, 15);
@@ -35,12 +39,14 @@ public class Topic_10_Handle_Custom_Dropdown {
 				
 				
 				// Wait cho việc tìm element. Findelement(s)
-				driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 				driver.manage().window().maximize();
+				tinh = "TP. Hồ Chí Minh";
+				khuvuc = "Khu vực I";
 	
 	}
 
-	@Test
+	//@Test
 		public void TC_01_JQuery() {
 		//"ul#number-menu li div"
 		//ul#number-menu li div
@@ -48,6 +54,42 @@ public class Topic_10_Handle_Custom_Dropdown {
 		selectItemCustomDropdown("ul#number-menu li div", "ul#number-menu li div", "5");
 			
 		
+	}
+	@Test
+	public void TC_02_JQuery2() {
+		selectItemCustomDropdownH("//div[@class='dropdown-menu show']/a", "//div[@class='dropdown-menu show']/a", "CITY L");
+		
+		select = new Select(driver.findElement(By.name("province")));
+		select.selectByVisibleText(tinh);
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), tinh);
+		select = new Select(driver.findElement(By.name("registration_fee")));
+		select.selectByVisibleText(khuvuc);
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), khuvuc);
+		
+	}
+
+	public void selectItemCustomDropdownH(String parentLocatorH, String childLocatorH, String expectedItemH) {
+		driver.get("https://www.honda.com.vn/o-to/du-toan-chi-phi");
+		sleepInsecond(5);
+		driver.findElement(By.xpath("//button[@class='btn btn-primary x']")).click();
+
+		driver.findElement(By.xpath("//button[@class='btn dropdown-toggle']")).click();
+
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(parentLocatorH)));
+		List<WebElement> allDropdownItems = driver.findElements(By.xpath(childLocatorH));
+
+		for (WebElement item : allDropdownItems) {
+			String actualTextItem = item.getText();
+			System.out.println("Item text = " + actualTextItem);
+			if (actualTextItem.equals(expectedItemH)) {
+				item.click();
+
+				break;
+
+			}
+		}
+
+		// - Item này sẽ đổ dữ liệu vào dropdown này => Verify chọn thành công
 	}
 	public void selectItemCustomDropdown (String parentLocator, String childLocator, String expectedItem) {
 		driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
@@ -82,16 +124,25 @@ public class Topic_10_Handle_Custom_Dropdown {
 		
 		// - Item này sẽ đổ dữ liệu vào dropdown này => Verify chọn thành công
 	}
-	@Test
-	public void TC_02() {
-		
-	}
-	@Test
+	
+	//@Test
 	public void TC_03() {
+		driver.get("https://www.honda.com.vn/o-to/du-toan-chi-phi");
+		
+		
+		driver.findElement(By.xpath("//button[@id='selectize-input']")).click();
+	}
+	public void sleepInsecond(long timeInsecond) {
+		try {
+			Thread.sleep(timeInsecond * 1000);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 	@AfterClass
 	public void afterClass() {
-		driver.quit();
+		//driver.quit();
 	}
 }
